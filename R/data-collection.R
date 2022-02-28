7e266945953aea1b2bf4c34bc24be82747b07fde
Ben Hanowell <ben@hanowell.me>
2022-02-27 20:35:31 -0800

    Add parity data

--- R/data-collection.R
+++ R/data-collection.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(HMDHFDplus)
 library(keyring)
 
-## Generate data ----
+## Generate data for Chile in 1992 ----
 CHLbirthsRRbo <- HMDHFDplus::readHFDweb(
   CNTRY = "CHL",
   item = "birthsRRbo",
@@ -30,3 +30,8 @@ CHLasfrRRbo <- HMDHFDplus::readHFDweb(
   dplyr::filter(Year == 1992) %>%
   dplyr::select(-Year)
 saveRDS(CHLasfrRRbo, "data/CHLasfrRRbo.rds")
+
+## Parity data ----
+parity_data <- rlnorm(n = 100000, meanlog = 1, sdlog = .3) %>%
+  tibble::tibble(parity = round(., 0))
+saveRDS(parity_data, "data/parity_data.rds")
